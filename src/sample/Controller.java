babb3e2563b5b46b5f4b9edbc714c50b8bd6a126
EugeneDar <evgenedar@mail.ru>
2020-07-20 22:59:05 +0300

    # secong try

---
+++ src/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    ImageView image;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
