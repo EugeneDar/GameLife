3f2d4cba7d971505e35b36acab717e96aedb532c
EugeneDar <evgenedar@mail.ru>
2020-07-21 13:40:48 +0300

    # game field

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,78 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Spinner;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Affine;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
     @FXML
-    Canvas canvas;
-    @FXML
-    Spinner<Integer> spinner123;
+    private Canvas canvas;
+
+    private Affine affine;
+
+    private Simulation simulation;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    public Controller() {
+
+        int fieldHeight = 60;
+        int fieldWidth = 80;
+
+        canvas = new Canvas(600, 800);
+
+        affine = new Affine();
+        affine.appendScale(800 / (fieldWidth * 1.0), 600 / (fieldHeight * 1.0));
+
+        simulation = new Simulation(fieldHeight, fieldWidth);
+
+    }
+
+    public void draw() {
+        GraphicsContext g = canvas.getGraphicsContext2D();
+        g.setTransform(affine);
+
+        g.setFill(Color.LIGHTGRAY);
+        g.fillRect(0, 0, 600, 800);
+
+        g.setFill(Color.BLUE);
+
+        for (int x = 0; x < this.simulation.width; x++) {
+            for (int y = 0; y < this.simulation.height; y++) {
+                if (this.simulation.isAlive(x, y) == 1) {
+                    g.fillRect(x, y, 1, 1);
+                }
+            }
+        }
+
+        g.setStroke(Color.GRAY);
+        g.setLineWidth(0.05);
+        for (int x = 0; x <= this.simulation.width; x++) {
+            g.strokeLine(x, 0, x, simulation.height);
+        }
+
+        for (int y = 0; y <= this.simulation.height; y++) {
+            g.strokeLine(0, y, simulation.width, y);
+        }
+
+    }
+
+    public void step (ActionEvent event) {
+        simulation.step();
+        draw();
+    }
+
 }
--- src/sample/Simulation.java
+++ src/sample/Simulation.java
@@ -1,4 +1,84 @@
 package sample;
 
 public class Simulation {
+
+    int height;
+    int width;
+    int[][] matrix;
+
+    public Simulation(int height, int width) {
+        this.width = width;
+        this.height = height;
+        matrix = new int[height][width];
+    }
+
+    public void printMatrix () {
+        for (int y = 0;y < height;y++) {
+            for (int x = 0;x < width;x++) {
+                if (isAlive(x, y) == 1) {
+                    System.out.print('*');
+                } else {
+                    System.out.print('.');
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public int countNeighbors (int x, int y) {
+        int count = 0;
+
+        count += isAlive(x + 1, y);
+        count += isAlive(x - 1, y);
+        count += isAlive(x, y + 1);
+        count += isAlive(x, y - 1);
+
+        count += isAlive(x + 1, y + 1);
+        count += isAlive(x + 1, y - 1);
+        count += isAlive(x - 1, y + 1);
+        count += isAlive(x - 1, y - 1);
+
+        return count;
+    }
+
+    public int isAlive (int x, int y) {
+        if (x < 0 || y < 0 || x >= width || y >= height) {
+            return 0;
+        }
+        return matrix[y][x];
+    }
+
+    public void setAlive (int x, int y) {
+        matrix[y][x] = 1;
+    }
+    public void setDead (int x, int y) {
+        matrix[y][x] = 0;
+    }
+
+    public void step () {
+        int[][] newMatrix = new int[height][width];
+
+        for (int y = 0;y < height;y++) {
+            for (int x = 0;x < width;x++) {
+                int count = countNeighbors(x, y);
+                if (isAlive(x, y) == 1) {
+                    if (count < 2 || count > 3) {
+                        newMatrix[y][x] = 0;
+                    } else {
+                        newMatrix[y][x] = 1;
+                    }
+                } else {
+                    if (count == 3) {
+                        newMatrix[y][x] = 1;
+                    } else {
+                        newMatrix[y][x] = 0;
+                    }
+                }
+            }
+        }
+
+        matrix = newMatrix;
+    }
 }
